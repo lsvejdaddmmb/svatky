e56363cc46adeef245071e8c59509a0a0b3756af
Libor Svejda <Libor.Svejda@unicorn.com>
2019-10-15 16:32:27 +0200

    nacteni index.html

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const http = require('http');
+const fs = require('fs');
 
 const PORT = 8888;
 
 function zpracujPozadavek(req, res) {
-
+    if (req.url === "/") {
+        fs.readFile("index.html", (err, data) => {
+            res.writeHead(200, {"Content-type":"text/html"});
+            res.end(data);
+        })
+    }
 }
 
 let srv = http.createServer(zpracujPozadavek);
